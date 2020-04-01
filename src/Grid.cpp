b1a2084ffbfe68c42bf75050d947e791bda17376
Niklas Persson <niklas.persson@gmail.com>
2020-04-01 11:24:16 +0200

    minor changes

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -4,18 +4,16 @@ Grid::Grid(int width, int height) {
     int startX = width / 2;
     int startZ = height / 2;
     for (int i = 0; i < width; i++) {
-        float step = 0.01f;
+        float step = 1.0f;
         auto start = Vertex { step * glm::vec3(-startX + i, 0, -startZ) };
         auto end = Vertex { step * glm::vec3(-startX + i, 0, -startZ + height) };
-        printf("(%.3f, %.3f, %.3f)  --->  (%.3f, %.3f, %.3f)\n", start.position.x, start.position.y, start.position.z, end.position.x, end.position.y, end.position.z);
         vertices.push_back(start);
         vertices.push_back(end);
     }
     for (int i = 0; i < height; i++) {
-        float step = 0.01f;
+        float step = 1.0f;
         auto start = Vertex { step * glm::vec3(-startX, 0, -startZ + i) };
         auto end = Vertex { step * glm::vec3(-startX + width, 0, -startZ + i) };
-        printf("(%.3f, %.3f, %.3f)  --->  (%.3f, %.3f, %.3f)\n", start.position.x, start.position.y, start.position.z, end.position.x, end.position.y, end.position.z);
         vertices.push_back(start);
         vertices.push_back(end);
     }
