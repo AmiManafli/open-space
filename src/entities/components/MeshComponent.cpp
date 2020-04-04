57d99a8f490e17d682b21d814a4e161e8dffa169
AmiManafli <amina.manafli@gmail.com>
2020-04-04 14:00:15 +0200

    more Windows fixes

--- src/entities/components/MeshComponent.cpp
+++ src/entities/components/MeshComponent.cpp
@@ -16,7 +16,8 @@ MeshComponent::~MeshComponent() {
 std::vector<MeshComponent *> MeshComponent::createMeshComponentsFromFile(std::string filename, ShaderProgram *shaderProgram) {
     auto meshes = std::vector<MeshComponent *>();
     Assimp::Importer importer;
-    const aiScene *scene = importer.ReadFile(std::filesystem::absolute(filename), aiProcess_Triangulate | aiProcess_FlipUVs);
+	std::string path = std::filesystem::absolute(filename).string();
+    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
         throw std::runtime_error(importer.GetErrorString());
