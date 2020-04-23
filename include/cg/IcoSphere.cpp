b7828e1be5198173f1375a6201b7f7609a634b8c
Niklas Persson <niklas.persson@gmail.com>
2020-04-24 00:23:39 +0200

    calculate normals (incorrect)

--- include/cg/IcoSphere.cpp
+++ include/cg/IcoSphere.cpp
@@ -72,5 +72,22 @@ void IcoSphere::generateMesh() {
     vertices[11] = Vertex { {0.0f, -radius, 0.0f} };
 
     // Update normals
-    
+    vertices[0].normal = { 0.0f, 1.0f, 0.0f };
+    for (int i = 1; i <= 5; i++) {
+        int iUpper = i;
+        int iUpperNext = i == 5 ? 1 : iUpper + 1;
+        int iLower = i + 5;
+        int iLowerNext = i == 5 ? 6 : iLower + 1;
+
+        Vertex upper = vertices[iUpper];
+        auto origin = vertices[iUpper].position - vertices[0].position;
+        auto right = vertices[iUpperNext].position - vertices[iUpper].position;
+        vertices[iUpper].normal = glm::normalize(glm::cross(right, origin));
+
+        Vertex lower = vertices[iLower];
+        origin = vertices[iLower].position - vertices[0].position;
+        right = vertices[iLowerNext].position - vertices[iLower].position;
+        vertices[iLower].normal = glm::normalize(glm::cross(right, origin));
+    }
+    vertices[11].normal = { 0.0f, -1.0f, 0.0f };
 }
