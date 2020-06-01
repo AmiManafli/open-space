6d3c5ef01a46cc55590176089ed531b12f243c70
Niklas Persson <equadon@gmail.com>
2020-06-01 19:15:27 +0200

    change resolution

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 //    auto app = new Application("Computer Graphics: Project", 2880, 1800);
-    auto app = new Application("Computer Graphics: Project", 1440, 900);
+    auto app = new Application("Computer Graphics: Project", 1920, 1080);
     app->init();
     app->run();
     delete app;
