97c31a82164a88e8f234c615d7dce85cfb977803
Niklas Persson <equadon@gmail.com>
2020-06-01 19:28:17 +0200

    do not use fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,7 @@
 #include <cg/Application.h>
 
 int main() {
-//    auto app = new Application("Computer Graphics: Project", 2880, 1800);
-    auto app = new Application("Computer Graphics: Project", 1920, 1080);
+    auto app = new Application("Computer Graphics: Project", 1200, 800);
     app->init();
     app->run();
     delete app;
